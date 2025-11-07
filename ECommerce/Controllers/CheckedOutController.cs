29618b0a40d751c1b1d4a176b1f7f9122f535ac7
ronAa21 <aaronpagente19@gmail.com>
2025-11-07 17:03:18 +0800

    Add project files.

---
+++ ECommerce/Controllers/CheckedOutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class CheckedOutController : Controller
+    {
+        // GET: CheckedOut
+        public ActionResult Checked()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECommerce/Controllers/RegisController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class RegisController : Controller
+    {
+        // GET: Regis
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
